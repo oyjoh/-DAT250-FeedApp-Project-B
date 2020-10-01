bd939ff34ba2e4a49b6041428151764ef9f734d5
Øyvind Straume Almli <oyvind.almli@gmail.com>
2020-10-01 13:03:10 +0200

    Added EntryController and PollController

--- src/main/java/com/dat250/FeedApp/controller/EntryController.java
+++ src/main/java/com/dat250/FeedApp/controller/EntryController.java
@@ -1,7 +1,47 @@
 package com.dat250.FeedApp.controller;
 
-import org.springframework.web.bind.annotation.RestController;
+import com.dat250.FeedApp.model.Entry;
+import com.dat250.FeedApp.model.Poll;
+import com.dat250.FeedApp.repository.EntryRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.rest.webmvc.ResourceNotFoundException;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 public class EntryController {
+
+    @Autowired
+    private EntryRepository entryRepository;
+
+    @GetMapping("/entry")
+    public List<Entry> getAllPersons(){
+        return entryRepository.findAll();
+    }
+
+    @PostMapping("/entry")
+    public Entry createPoll(@Validated @RequestBody Entry entry){
+        return entryRepository.save(entry);
+    }
+
+    @PutMapping("/entry/{entryId}")
+    public Entry updatePoll(@PathVariable Long entryId, @Validated @RequestBody Entry entryRequest){
+        return entryRepository.findById(entryId).map(entry -> {
+            //entry.setNumber(entryRequest.getNumber());
+            entry.setValue(entryRequest.getValue());
+            return entryRepository.save(entry);
+        }).orElseThrow(() -> new ResourceNotFoundException("EntryId: " + entryId + " not found"));
+    }
+
+    @DeleteMapping("/entry/{entryId}")
+    public ResponseEntity<?> deleteEntry(@PathVariable Long entryId){
+        return entryRepository.findById(entryId).map(entry -> {
+            entryRepository.delete(entry);
+            return ResponseEntity.ok().build();
+        }).orElseThrow(() -> new ResourceNotFoundException("EntryId: " + entryId + " not found"));
+    }
 }
+
--- src/main/java/com/dat250/FeedApp/controller/PollController.java
+++ src/main/java/com/dat250/FeedApp/controller/PollController.java
@@ -1,7 +1,46 @@
 package com.dat250.FeedApp.controller;
 
-import org.springframework.web.bind.annotation.RestController;
+import com.dat250.FeedApp.model.Poll;
+import com.dat250.FeedApp.repository.PollRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.rest.webmvc.ResourceNotFoundException;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 public class PollController {
+
+    @Autowired
+    private PollRepository pollRepository;
+
+    @GetMapping("/poll")
+    public List<Poll> getAllPersons(){
+        return pollRepository.findAll();
+    }
+
+    @PostMapping("/poll")
+    public Poll createPoll(@Validated @RequestBody Poll poll){
+        return pollRepository.save(poll);
+    }
+
+    @PutMapping("/poll/{pollId}")
+    public Poll updatePoll(@PathVariable Long pollId, @Validated @RequestBody Poll pollRequest){
+        return pollRepository.findById(pollId).map(poll -> {
+            poll.setSummary(pollRequest.getSummary());
+            poll.setIsPublic(pollRequest.getIsPublic());
+            poll.setEntries(pollRequest.getEntries());
+            return pollRepository.save(poll);
+        }).orElseThrow(() -> new ResourceNotFoundException("PollId: " + pollId + " not found"));
+    }
+
+    @DeleteMapping("/poll/{pollId}")
+    public ResponseEntity<?> deletePoll(@PathVariable Long pollId){
+        return pollRepository.findById(pollId).map(poll -> {
+            pollRepository.delete(poll);
+            return ResponseEntity.ok().build();
+        }).orElseThrow(() -> new ResourceNotFoundException("PollId: " + pollId + " not found"));
+    }
 }
