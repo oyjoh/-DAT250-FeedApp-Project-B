9c79a980c77f78c22a9b950cb4f7c3cfff6bd942
Øyvind Straume Almli <oyvind.almli@gmail.com>
2020-10-01 12:39:07 +0200

    Added @Autowired to PersonRepository, fixing the 500 error

--- src/main/java/com/dat250/FeedApp/controller/PersonController.java
+++ src/main/java/com/dat250/FeedApp/controller/PersonController.java
@@ -3,6 +3,7 @@ package com.dat250.FeedApp.controller;
 
 import com.dat250.FeedApp.model.Person;
 import com.dat250.FeedApp.repository.PersonRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.rest.webmvc.ResourceNotFoundException;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
@@ -13,6 +14,7 @@ import java.util.List;
 @RestController
 public class PersonController {
 
+    @Autowired
     private PersonRepository personRepository;
 
     @GetMapping("/people")
