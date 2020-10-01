63afa4ca885d7cc408e555f9d5501847cd46c270
oyjoh <oyvind.bre.joh@gmail.com>
2020-10-01 11:54:15 +0200

    first commit

---
+++ src/main/java/com/dat250/FeedApp/FeedAppApplication.java
@@ -0,0 +1,13 @@
+package com.dat250.FeedApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FeedAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FeedAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dat250/FeedApp/FeedAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dat250.FeedApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FeedAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
