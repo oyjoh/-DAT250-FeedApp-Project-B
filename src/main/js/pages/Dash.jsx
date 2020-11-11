c69124f2904a20f8da6d4d25b86684feeb8dbae5
Øyvind Straume Almli <oyvind.almli@gmail.com>
2020-11-11 14:01:00 +0100

    working on Dash

--- src/main/js/pages/Dash.jsx
+++ src/main/js/pages/Dash.jsx
@@ -1,5 +1,5 @@
 import Container from "@material-ui/core/Container";
-import React from "react";
+import React, {useEffect, useState} from "react";
 import TableContainer from "@material-ui/core/TableContainer";
 import Table from "@material-ui/core/Table";
 import TableHead from "@material-ui/core/TableHead";
@@ -13,6 +13,7 @@ import Toolbar from "@material-ui/core/Toolbar";
 import IconButton from "@material-ui/core/IconButton";
 import Typography from "@material-ui/core/Typography";
 import VoteButtonComponent from "../components/VoteButtonComponent.jsx";
+import axios from "axios";
 
 const useStyles = makeStyles({
     table: {
@@ -32,8 +33,52 @@ const rows = [
     createData('854 153', 11, 10, "52.5%", "Do you like The Beach Boys?", "06/06/21"),
 ];
 
-function Dash() {
+const [pollList, setPollList] = useState([]);
+
+
+const person = {
+    createdAt: "2020-11-11T10:15:48.535+00:00",
+    updatedAt: "2020-11-11T10:15:48.647+00:00",
+    personId: 60,
+    name: "ola1605089748209",
+    email: "ola1605089748209@gmail.com",
+    cookie: "ed9a9476-4e59-4b96-a01c-d1fde887b837",
+    roles: [
+    "USER"
+],
+    polls: [
+        74,
+        75,
+        76,
+        77
+    ],
+    entries: [
+        105,
+        106,
+        107,
+        108,
+        109,
+        110
+    ],
+    enabled: true,
+    admin: false,
+    username: "60",
+    user: true
+}
+const getPoll = async (pollId) => {
+    return await axios.get('/api/polls/' + pollId)
+}
+
+
+const Dash = () => {
     const classes = useStyles();
+
+    useEffect(() => {
+        for (const pollId of person.polls) {
+            const newPollList = pollList.concat(getPoll(pollId));
+            setPollList(newPollList);
+        }
+    });
 
     return (
         <div>
