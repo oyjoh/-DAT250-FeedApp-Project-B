0a5dc95f57ed6e935e76d9f06250c26e0e4f6b42
Gunnar <Gunnar.Kleiven@student.uib.no>
2020-11-11 12:15:31 +0100

    Scheduled class that gets new dweets from a namelist

--- src/main/java/com/dat250/FeedApp/dweeting/Dweet.java
+++ src/main/java/com/dat250/FeedApp/dweeting/Dweet.java
@@ -2,12 +2,19 @@ package com.dat250.FeedApp.dweeting;
 
 import com.google.gson.JsonObject;
 
+import java.util.Objects;
+
 public class Dweet {
 
 
     public String thing;
     public String created;
     public JsonObject content;
+
+    // TODO check if dweet is at correct format, with correct command and payload
+    public String getCommand() {
+        return content.get("command").toString();
+    }
 
 
     @Override
@@ -16,5 +23,20 @@ public class Dweet {
                 "\tthing = '" + thing + ", \n" +
                 "\tcreated = '" + created + ",\n" +
                 "\tcontent = " + content;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Dweet dweet = (Dweet) o;
+        return Objects.equals(thing, dweet.thing) &&
+                Objects.equals(created, dweet.created) &&
+                Objects.equals(content, dweet.content);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(thing, created, content);
     }
 }
